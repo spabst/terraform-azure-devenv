592323cc237088ef40ad5a84164d5c0afd308219
Sebastian Pabst <sebapabst@gmail.com>
2024-02-02 22:53:25 +0100

    added network security group rules

--- main.tf
+++ main.tf
@@ -47,3 +47,19 @@ resource "azurerm_network_security_group" "mtc-nsg" {
     environment = "dev"
   }
 }
+
+resource "azurerm_network_security_rule" "mtc-dev-rule" {
+  name                        = "mtc-dev-rule"
+  resource_group_name         = azurerm_resource_group.mtc-rg.name
+  network_security_group_name = azurerm_network_security_group.mtc-nsg.name
+
+  priority                   = 100
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "*"
+  source_port_range          = "*"
+  destination_port_range     = "*"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+
+}
