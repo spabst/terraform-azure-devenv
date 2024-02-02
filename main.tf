9440583090b93e5f1e34948f655de0191be243cf
Sebastian Pabst <sebapabst@gmail.com>
2024-02-02 14:48:21 +0100

    added a subnet

--- main.tf
+++ main.tf
@@ -31,3 +31,10 @@ resource "azurerm_virtual_network" "mtc-vn" {
     environment = "dev"
   }
 }
+
+resource "azurerm_subnet" "mtc-subnet" {
+  name                = "mtc-subnet"
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+  virtual_network_name = azurerm_virtual_network.mtc-vn.name
+  address_prefixes       = ["10.123.1.0/24"]
+}
