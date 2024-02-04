615118c71868a288be57e233e4c1c6cb6ce95485
Sebastian Pabst <sebapabst@gmail.com>
2024-02-04 23:22:35 +0100

    added data source and output

--- main.tf
+++ main.tf
@@ -137,3 +137,12 @@ resource "azurerm_linux_virtual_machine" "mtc-vm" {
     environment = "dev"
   }
 }
+
+data "azurerm_public_ip" "mtc-ip-data"{
+  name = azurerm_public_ip.mtc-ip.name
+  resource_group_name = azurerm_resource_group.mtc-rg.name
+}
+
+output "public_ip_address"{
+  value = "${azurerm_linux_virtual_machine.mtc-vm.name}:${data.azurerm_public_ip.mtc-ip-data.ip_address}"
+}
